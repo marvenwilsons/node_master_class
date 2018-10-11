7ff46d17dd9e8ec57f80397dd0c14f6693e341ae
Marven Wilson Donque <marvenwilsons@gmail.com>
2018-10-11 12:54:49 -0400

    added a URL  parsing logic, that receives url input from the client then logs the input to the terminal

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 
 // dependencies
 const http = require('http')
+const url = require('url')
 
 // the server should respond to all request with a string
 const server = http.createServer((req,res) => {
+    // get the url and parse it
+    const parsedUrl = url.parse(req.url, true)
+
+    // get the path from the url
+    const path = parsedUrl.pathname
+    const trimedPath = path.replace(/^\/+|\/+$/g,'')
+
+    // send the response
     res.end('hello world\n')
+
+    // log the request path
+    console.log(`request received: ${trimedPath === '' ? 'home' : trimedPath}`)
+
 })
 
 // start the server, and have it listen on port 3000
