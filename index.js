5e376f425284418c6e99ca6c4d72c691f76bb4d1
Marven Wilson Donque <marvenwilsons@gmail.com>
2018-10-11 20:59:37 -0400

    Added parsing payload from user if any

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
 // dependencies
 const http = require('http')
 const url = require('url')
+const stringDecoder = require('string_decoder').StringDecoder
 
 // the server should respond to all request with a string
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
     // get the url and parse it
     const parsedUrl = url.parse(req.url, true)
 
     // get the path from the url
     const path = parsedUrl.pathname
-    const trimedPath = path.replace(/^\/+|\/+$/g,'')
+    const trimedPath = path.replace(/^\/+|\/+$/g, '')
 
     // get the query string as an object
     // url?key=value <- from client
@@ -23,15 +24,32 @@ const server = http.createServer((req,res) => {
     // Get the headers as an object
     const headers = req.headers
 
-    // send the response
-    res.end('hello world\n')
+    // Get payload, if any, POST request
+    const decoder = new stringDecoder('utf-8')
+    let buffer = ''
+    req.on('data', (data) => {
+        buffer += decoder.write(data)
+    })
+    req.on('end', () => {
+        buffer += decoder.end()
 
-    // log the request path
-    console.log(headers)
+        // send the response
+        res.end('hello world\n')
+
+        // log the request path
+        let r = {
+            parsedUrl: parsedUrl,
+            trimedPath: trimedPath,
+            method: method,
+            header: headers,
+            payload: buffer
+        }
+        console.log(r)
+    })
 
 })
 
 // start the server, and have it listen on port 3000
-server.listen(3000,(req,res)=> {
+server.listen(3000, (req, res) => {
     console.log('The server is litening to port 3000 now')
 })
